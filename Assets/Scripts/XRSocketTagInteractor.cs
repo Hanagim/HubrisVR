3d8371a670d64c84766c80ffd06c7063598c5928
Jonas Kersten <jfhkersten@gmail.com>
2025-04-08 23:17:02 +0200

    Clean up Assets, setting up Push Button
    Deleted files that we;re not using (for now) and setting up Push Button functionality

--- Assets/Scripts/XRSocketTagInteractor.cs
+++ Assets/Scripts/XRSocketTagInteractor.cs
@@ -10,6 +10,11 @@ public class XRSocketTagInteractor : XRSocketInteractor
 
     public override bool CanHover(IXRHoverInteractable interactable)
     {
-        return base.CanHover(interactable);
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
     }
 }
